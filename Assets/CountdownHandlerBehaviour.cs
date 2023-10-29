902f04ea3d12aad613caca7a6731018e322993c7
Tonyy456 <ajdalesandro0115@gmail.com>
2023-10-28 23:48:23 -0400

    countdown routine pt 1

--- Assets/CountdownHandlerBehaviour.cs
+++ Assets/CountdownHandlerBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,4 +7,37 @@ public class CountdownHandlerBehaviour : MonoBehaviour
 {
     [SerializeField] private GameObject panel;
     [SerializeField] private TMPro.TMP_Text text;
+    [SerializeField] private float cdTime = 5f;
+
+    private IEnumerator currentRoutine;
+
+    public void StartCountdown()
+    {
+        if (currentRoutine == null)
+        {
+            currentRoutine = countdownRoutine(cdTime);
+            StartCoroutine(currentRoutine);
+        } 
+    }
+
+
+    public IEnumerator countdownRoutine(float startSeconds)
+    {
+        float remainingTime = startSeconds;
+        while(remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+            SetText(remainingTime);
+        }
+        currentRoutine = null;
+        yield return null;
+    }
+
+    public void SetText(float seconds)
+    {
+        // Format as "mm:ss"
+        TimeSpan timeSpan = TimeSpan.FromSeconds(seconds);
+        string formattedTime = string.Format("{0:D2}:{1:D2}", timeSpan.Minutes, timeSpan.Seconds);
+        text.text = formattedTime;
+    }
 }
